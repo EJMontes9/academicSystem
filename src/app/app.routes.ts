cd64a58e4d15fe44e0845cf09d1b8843e5c96b85
Darkod <erickmontes77.jm@gmail.com>
2025-02-03 20:50:12 -0500

    Add authentication service and main template components
    Introduce `AuthService` with login, logout, and token handling logic, along with unit tests. Add the main template structure (HTML, CSS) and its corresponding component with initial test setup for the application layout.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {MainComponent} from "./templates/main/main.component";
+import {SecretaryComponent} from "./pages/secretary/secretary.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent,  children: [
+            {path: '', redirectTo: 'secretary', pathMatch: 'full'},
+            {path: 'secretary', component: SecretaryComponent},
+        ]
+    }
+];
